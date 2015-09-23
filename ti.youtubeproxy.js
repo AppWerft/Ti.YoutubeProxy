a4f91b8eb193a406efb6700dbcb4bbb6d9b43caa
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-09-23 14:11:20 +0200

    Init

---
+++ ti.youtubeproxy.js
@@ -0,0 +1,61 @@
+var HTTPRequest = require('./lib/ts.httprequest');
+var VideoProvider = require('./lib/ts.videoprovider');
+var Promise  = require('./lib/ti.promises');
+
+var GOOGLEAPIKEY = Ti.App.Properties.getString('GOOGLEAPIKEY');
+
+var _getPlaylistsByChannelId = function(channelid) {
+	var defer = Promise.defer();
+	function handleResponse(response) {
+        var result = JSON.parse(response); 
+        var items = result.items.map(function(item) {
+				return {
+					title : item.snippet.title,
+					data : item.id
+				};
+			});
+        defer.resolve(items);
+    }
+    function handleError(e) { defer.reject(e); }
+	(new HTTPRequest({
+            url: "https://www.googleapis.com/youtube/v3/playlists?maxResults=50&part=snippet%2CcontentDetails&channelId="+channelid+"&key=" + GOOGLEAPIKEY",
+            method: "GET",
+            success: handleResponse,
+            error: handleError,
+    })).send();
+};
+ 
+var _getVideosByplaylistId = function(playlistid) {
+	var defer = Promise.defer();
+	function handleResponse(response) {
+        var playlists = JSON.parse(this.responseText);
+			defer.resolve(playlists.items.map(function(v) {
+				return {
+					image : v.snippet.thumbnails.default.url.replace(/default/, 'hqdefault'),
+					title : v.snippet.title.replace(/^[\s]+/g, ''),
+					id : v.snippet.resourceId.videoId,
+					description : v.snippet.description.split('\n')[0],
+				};
+			}));
+    }
+    function handleError(e) { defer.reject(e); }
+	(new HTTPRequest({
+            url: 'https://www.googleapis.com/youtube/v3/playlistItems?&maxResults=50&part=snippet%2CcontentDetails%2Cstatus&playlistId=' + playlistid + '&key=' + GOOGLEAPIKEY",
+            method: "GET",
+            success: handleResponse,
+            error: handleError,
+    })).send();
+};
+
+var getUrlById = function(id) {
+	var defer = Promise.defer();
+	VideoProvider.getVideoUrl('youtube', id).then(
+        function onSuccess(url) { defer.resolve(url); }, 
+        function onError(e) { Ti.API.error(e); }
+    );
+};
+
+
+exports.getPlaylistsByChannelId = _getPlaylistsByChannelId;
+exports.getVideosByplaylistId = _getVideosByplaylistId;
+exports.getUrlById = _getUrlById;
